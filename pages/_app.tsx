9592ec2b880c18420e50fab025fd70dd7a66c7c3
Anny Confessor <anny.confessor@rockcontent.com>
2021-12-20 10:53:58 -0300

    chore: add routes and theme

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { ReactChild } from "react"
+import ThemeContainer from "./../src/theme"
+
+interface MyAppProps {
+  Component: any,
+  pageProps: ReactChild
+}
+
+function MyApp({ Component, pageProps }: MyAppProps) {
+  return (
+    <ThemeContainer>
+      <Component {...pageProps} />
+    </ThemeContainer>
+  )
+}
+
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { Flex, Image, Button, Text } from '@chakra-ui/core'
+import { Flex, Box, Button, Input, Text } from '@chakra-ui/core'
 import axios from 'axios'
+import { Center } from '@chakra-ui/react'
 
 export default function Home() {
     const [ email, setEmail ] = useState()
@@ -10,6 +11,21 @@ export default function Home() {
     }
 
     return(
-        <div>Oi, eu sou a homepage</div>
+        <Flex
+            className='login-container'
+            color='#7393B3'
+            height='100vh'
+            justifyContent='center'
+            alignItems='center'
+            backgroundColor='#202124'
+        >
+            <Box className='input-container'>
+                <Center bg='pink.300' h='100px' color='pink.100'>
+                    Login
+                    <Input variant='outline' placeholder='Senha' />
+                    <Button color='#0BC5EA'>Button</Button>
+                </Center>
+            </Box>
+        </Flex>
     )
  }
